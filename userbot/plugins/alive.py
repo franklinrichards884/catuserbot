6105538dccd0d4fa2c01a2cd02dd8531adde774c
franklinrichards884 <67375816+franklinrichards884@users.noreply.github.com>
2020-12-18 17:37:49 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -62,15 +62,6 @@ async def amireallyalive(alive):
     await alive.delete()
 
 
-# UniBorg Telegram UseRBot
-# Copyright (C) 2020 @UniBorg
-# This code is licensed under
-# the "you can't use this for anything - public or private,
-# unless you know the two prime factors to the number below" license
-# 543935563961418342898620676239017231876605452284544942043082635399903451854594062955
-# വിവരണം അടിച്ചുമാറ്റിക്കൊണ്ട് പോകുന്നവർ
-# ക്രെഡിറ്റ് വെച്ചാൽ സന്തോഷമേ ഉള്ളു..!
-# uniborg
 
 
 def check_data_base_heal_th():
